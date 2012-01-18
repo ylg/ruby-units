1a0c8b539dffe33b02780ec217a0cf75627c9399
Yuri Gadow <yuri@gadow.name>
2012-01-18 15:03:27 -0700

    Remove specs that covered date and time functionality.

--- spec/ruby-units/string_spec.rb
+++ spec/ruby-units/string_spec.rb
@@ -14,7 +14,6 @@ describe String do
     specify { ("%0.2f" % subject).should == "1.23 m/s^2"}
     specify { ("%0.2f km/h^2" % subject).should == "15999.90 km/h^2"}
     specify { ("km/h^2" % subject).should == "15999.9 km/h^2"}
-    specify { ("%H:%M:%S" % Unit("1.5 h")).should == "01:30:00"}
   end
   
 end
